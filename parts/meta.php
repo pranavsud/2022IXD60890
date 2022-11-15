65272d9cc7f1b010a828299d8949235da4b885d2
pranavsud <112988627+pranavsud@users.noreply.github.com>
2022-11-15 13:19:01 -0800

    Add files via upload

---
+++ parts/meta.php
@@ -0,0 +1,7 @@
+	<meta charset="utf-8">
+	<base href="http://pranavsudhakar.com/aau/ixd608/sudhakar.pranav/">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
---
+++ parts/navbar.php
@@ -0,0 +1,18 @@
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Store</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<div class="flex-none nav nav-flex">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="product_cart.php">Cart</a></li>
+
+				</ul>
+			</div>
+
+		</div>
+	</header>
